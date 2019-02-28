538cab7f2688ee0b019233d5fab7cf286591d450
Jerry Ejonavi <jerryasih@gmail.com>
2019-02-28 02:02:08 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// jQuery code will go here
+// FIRST TASK:
+// init the jQuery
+// stop the form from sending when the submit button is clicked
+// show an alert that will
+// warn if the email address field is empty
+// else say "Good to go!" if there is an email address
+// in the field
